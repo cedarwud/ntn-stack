c5e578d3beb1eeb63317f87e8dfd554db47b8ba4
Ben <cedarwud@gmail.com>
2025-06-28 12:53:21 +0000

    high2
    Former-commit-id: a2a8a9104753e45327558186a179fe93c2565665

---
+++ simworld/backend/app/domains/simulation/services/rendering/__init__.py
@@ -0,0 +1,8 @@
+"""
+渲染引擎模組
+包含 3D 場景渲染、圖像處理等功能
+"""
+
+from .rendering_service import RenderingService
+
+__all__ = ["RenderingService"]
---
+++ simworld/backend/app/domains/simulation/services/scene/__init__.py
@@ -0,0 +1,8 @@
+"""
+場景管理模組
+包含場景健康度檢查、檔案管理等功能
+"""
+
+from .scene_management_service import SceneManagementService
+
+__all__ = ["SceneManagementService"]
