3b78f1f105924fdcd8032fa4b26ad707cfa0ec92
Ben <cedarwud@gmail.com>
2025-07-11 10:13:06 +0000

    doc

--- netstack/netstack_api/models/__init__.py
+++ netstack/netstack_api/models/__init__.py
@@ -1,3 +1,66 @@
 """
 NetStack API 數據模型包
 """
+
+# 核心模型
+from .requests import *
+from .responses import *
+from .unified_models import *
+
+# 專門模型
+from .uav_models import *
+from .mesh_models import *
+from .performance_models import *
+from .sionna_models import *
+from .physical_propagation_models import *
+from .ueransim_models import *
+
+# RL 訓練模型
+from .rl_training_models import (
+    # 枚舉類型
+    TrainingStatus,
+    AlgorithmType,
+    EnvironmentType,
+    ModelStatus,
+    # 核心資料庫模型
+    RLTrainingSession,
+    RLTrainingEpisode,
+    RLAlgorithmPerformance,
+    RLModelVersion,
+    RLHyperparameterConfig,
+    # 響應模型
+    TrainingSessionResponse,
+    TrainingEpisodeResponse,
+    AlgorithmPerformanceResponse,
+    ModelVersionResponse,
+    # 請求模型
+    StartTrainingRequest,
+    UpdateTrainingRequest,
+    CreateModelVersionRequest,
+    HyperparameterConfigRequest,
+)
+
+__all__ = [
+    # 核心模型
+    "SystemStatus",
+    "HealthResponse",
+    "UEStatsResponse",
+    # RL 訓練模型
+    "TrainingStatus",
+    "AlgorithmType",
+    "EnvironmentType",
+    "ModelStatus",
+    "RLTrainingSession",
+    "RLTrainingEpisode",
+    "RLAlgorithmPerformance",
+    "RLModelVersion",
+    "RLHyperparameterConfig",
+    "TrainingSessionResponse",
+    "TrainingEpisodeResponse",
+    "AlgorithmPerformanceResponse",
+    "ModelVersionResponse",
+    "StartTrainingRequest",
+    "UpdateTrainingRequest",
+    "CreateModelVersionRequest",
+    "HyperparameterConfigRequest",
+]
