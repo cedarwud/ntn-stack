0214b84a32c7042cc70a38aae473e305ef198920
Ben <cedarwud@gmail.com>
2025-07-19 11:51:57 +0000

    Phase 2.1

---
+++ simworld/frontend/src/components/domains/measurement/charts/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Measurement Event Charts Index
+ * 測量事件圖表組件導出
+ */
+
+// 原有組件
+export { default as EventA4Chart } from './EventA4Chart'
+export { default as EventA4Viewer } from './EventA4Viewer'
+export { default as EventD1Chart } from './EventD1Chart'
+export { default as EventD1Viewer } from './EventD1Viewer'
+export { default as EventD2Chart } from './EventD2Chart'
+export { default as EventD2Viewer } from './EventD2Viewer'
+export { default as EventT1Chart } from './EventT1Chart'
+export { default as EventT1Viewer } from './EventT1Viewer'
+
+// Pure Charts
+export { default as PureA4Chart } from './PureA4Chart'
+export { default as PureD1Chart } from './PureD1Chart'
+export { default as PureD2Chart } from './PureD2Chart'
+export { default as PureT1Chart } from './PureT1Chart'
+
+// 增強版組件 - 新加入
+export { default as EnhancedD2Chart } from './EnhancedD2Chart'
+export { default as EnhancedD2Viewer } from './EnhancedD2Viewer'
+
+// 共用組件
+export { default as NarrationPanel } from './NarrationPanel'
