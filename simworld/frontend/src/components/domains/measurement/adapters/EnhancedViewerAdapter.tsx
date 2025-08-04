ebf6dc8add5ee179310c05ef8201ec36df836aaf
Ben <cedarwud@gmail.com>
2025-08-04 17:54:56 +0000

    phase 3

--- simworld/frontend/src/components/domains/measurement/adapters/EnhancedViewerAdapter.tsx
+++ simworld/frontend/src/components/domains/measurement/adapters/EnhancedViewerAdapter.tsx
@@ -3,12 +3,13 @@
  *
  * 適配 Modal 傳遞的 props 與 Enhanced 組件期望的介面
  * 確保 Enhanced 組件能在現有的 Modal 系統中正常工作
+ * 
+ * 注意：D2 事件已移除，統一由 EventD2Viewer 通過 📊 D2 事件監控 按鈕訪問
  */
 
 import React from 'react'
-// 🔧 臨時切換到舊版工作正常的實現
+// 只需要 A4 事件組件用於 MeasurementEventsModal
 import EventA4Viewer from '../charts/EventA4Viewer'
-import EventD2Viewer from '../charts/EventD2Viewer'
 
 // Modal 傳遞的 props 介面
 interface ModalProps {
@@ -19,7 +20,7 @@ interface ModalProps {
     isDarkTheme?: boolean
 }
 
-// 🔧 切換到舊版工作正常的組件
+// A4 事件適配器 - 用於 MeasurementEventsModal 中的 A4 信號切換事件
 export const AdaptedEnhancedA4Viewer: React.FC<ModalProps> = (props) => {
     return (
         <EventA4Viewer
@@ -31,13 +32,4 @@ export const AdaptedEnhancedA4Viewer: React.FC<ModalProps> = (props) => {
     )
 }
 
-export const AdaptedEnhancedD2Viewer: React.FC<ModalProps> = (props) => {
-    return (
-        <EventD2Viewer
-            onReportLastUpdateToNavbar={props.onReportLastUpdateToNavbar}
-            reportRefreshHandlerToNavbar={props.reportRefreshHandlerToNavbar}
-            reportIsLoadingToNavbar={props.reportIsLoadingToNavbar}
-            isDarkTheme={props.isDarkTheme}
-        />
-    )
-}
+// 移除 AdaptedEnhancedD2Viewer - D2 事件統一由 EventD2Viewer 處理
